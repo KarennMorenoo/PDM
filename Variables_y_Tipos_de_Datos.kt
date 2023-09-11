cace7623c5106ddf5efe290784de6b8cbd44837f
Karen Moreno <144639226+KarennMorenoo@users.noreply.github.com>
2023-09-10 23:18:15 -0600

    Create Variables_y_Tipos_de_Datos.kt

---
+++ Variables_y_Tipos_de_Datos.kt
@@ -0,0 +1,16 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+    var Decimal = 2.5;
+    println(Decimal)
+    var floatDecimal = 2.5;
+    println(floatDecimal)
+    val pi = 3.1416;
+    val nuevoDecimal = 2 * Decimal;
+    val perimetro = pi * nuevoDecimal;
+    val resultado = perimetro;
+    val stringresultado = "El perimetro del circulo es: $resultado"
+    print(stringresultado)
+}
