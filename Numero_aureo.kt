87eaa7f76b4ead2f85e9d8a9ebd6707cf49abb39
Karen Moreno <144639226+KarennMorenoo@users.noreply.github.com>
2023-10-03 22:48:29 -0600

    Create Numero_aureo.kt

---
+++ Numero_aureo.kt
@@ -0,0 +1,16 @@
+fun main 0 {
+val radius = 4f
+val area = circleArea (radius)
+printin ("El area del circulo es: $area")
+printin (getPi()
+printPHI()
+}
+fun circleArea (radius :Float) : Float {
+return Pi*radius.pow ( n: 2)
+}
+fun getPi(): Float{
+return Pi
+}
+fun printPHI(){
+println ("El numero aureo vale $PHI")
+}
